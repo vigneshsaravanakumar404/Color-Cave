d3f0f2620bc1c4abcd84460edc501e1e34db9559
amarsheth19 <amar.sheth19@gmail.com>
2024-03-20 11:41:40 -0400

    Fixed RoomLoader

--- RoomLoader.java
+++ RoomLoader.java
@@ -1,8 +1,9 @@
 public class RoomLoader extends AbstractRoomLoader{
 
     public RoomLoader(){
+        cave = new CaveData();
         load();
-        serialize("YellowBlueStart.ser");
+        serialize("RedGreenStart.ser");
     }
 
     @Override
@@ -10,13 +11,14 @@ public class RoomLoader extends AbstractRoomLoader{
         // TODO Auto-generated method stub
         // Load up all rooms and doors and add to
         // your cave.  Also set start and end rooms
-        Room r1 = new Room(">>>  Room 1  <<<","Start");
+        Room r1 = new Room(">>>  Room 1  <<<","");
         Room r2 = new Room(">>>  Room 2  <<<","");
         Room r3 = new Room(">>>  Room 3  <<<","");
         Room r4 = new Room(">>>  Room 4  <<<","");
         Room r5 = new Room(">>>  Room 4  <<<","");
         Room r6 = new Room(">>>  Room 4  <<<","");
-        Room r7 = new Room(">>>  Room 4  <<<","End");
+        Room r7 = new Room(">>>  Room 4  <<<","");
+        
         r1.addDoor(Door.RED,r3);
         r1.addDoor(Door.BLUE,r2);
 
@@ -31,24 +33,16 @@ public class RoomLoader extends AbstractRoomLoader{
 
         r6.addDoor(Door.BLUE, r7);
 
-        start = r1;
-        end = r7;
-    }
+        cave.addRoom(r1);
+        cave.addRoom(r2);
+        cave.addRoom(r3);
+        cave.addRoom(r4);
+        cave.addRoom(r5);
+        cave.addRoom(r6);
+        cave.addRoom(r7);
 
-    @Override
-    public Room getStart() {
-        return start;
-    }
-
-    @Override
-    public Room getEnd() {
-        return end;
-    }
-    
-    public static void main(String[] args){
-        AbstractRoomLoader rL = new RoomLoader();
-        rl = rl.deserialize("YellowBlueStart.ser");
-
+        cave.setStart(r1);
+        cave.setEnd(r7);
     }
 
 }
